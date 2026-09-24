0dd882873d68a6c0a2a51bd485cb139b6826c074
happykirtania-netizen <happykirtania-netizen@users.noreply.github.com>
2026-09-25 00:34:12 +0530

    chore: bump service worker cache version to force purge stale bundles

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'foundators-v1';
+const CACHE_NAME = 'foundators-v2';
 const PRECACHE = ['/', '/home', '/discover', '/messages', '/login', '/icon.svg'];
 
 self.addEventListener('install', (e) => {
